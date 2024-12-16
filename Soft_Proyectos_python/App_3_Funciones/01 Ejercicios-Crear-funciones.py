348f884f53fac411057a0c865b74253637fe4902
Joan Paredes <jparedesy@gmail.com>
2024-12-16 17:08:33 -0500

    Se está agregando ejercicios para las funciones

--- Soft_Proyectos_python/App_3_Funciones/01 Ejercicios-Crear-funciones.py
+++ Soft_Proyectos_python/App_3_Funciones/01 Ejercicios-Crear-funciones.py
@@ -6,7 +6,7 @@ def calcular_area_cuadrado(lado):
 
 
 
-#Crea una funcion para calcular el área de un rectangulo
+#Crea una función para calcular el área de un rectangulo
 #  a = lado* ancho
 def calcular_area_rectangulo(lado, ancho):
     return lado * ancho
@@ -32,3 +32,64 @@ lado_rec = 15
 ancho_rec = 20
 resultado_recttangulo = calcular_area_rectangulo(lado_rec, ancho_rec)
 print(f"El resultado obtenido para el área del rectangulo es: {resultado_recttangulo}")
+
+
+
+
+ 
+
+
+
+#Crear una función donde pases los siguientes paramentros:
+# texto 
+# indice_inicio
+# indice_fin
+#Lo que debes hacer es al texto que pases a la funcion te devuelva una parte de ella usando los otros 2 paramentros de indices 
+
+
+#Usando slicing
+def slicing_texto(texto, indice_ini, indice_fin):
+     
+     #Usaremos los indices para poder hacer uso del slicing
+     return texto[indice_ini:indice_fin]
+
+#Declaramos nuestras variables con la información correspondiente:
+texto = "Estamos comprometidos en aprender constantemente"
+indice_ini = 0
+indice_fin = 21
+
+resultado_slicing = slicing_texto(texto, indice_ini, indice_fin)
+print(f"Resultado del texto recortado: {resultado_slicing}")
+
+
+
+
+#Crea una función que reciba una lista de números y devuelva la suma de todos ellos.
+
+def sumar_lista(lista):
+    #usando solo la funcion sum, nos permite sumar toda la lista, ahorrandonos hacer un bucle for y recorrer toda la lista
+    #e ir sumando 1 x 1
+    return sum(lista)
+
+
+mi_lista = [5, 10, 50]
+resultado_lista_sum = sumar_lista(mi_lista)
+print(f"Resultado de la lista sumada: {resultado_lista_sum}")
+
+
+
+#Crea una función que permita contar vocales en un texto
+
+def contar_vocales(texto):    
+    sumando = 0
+    #recoremos el texto
+    for letra in texto:
+        #validamos si es un vocal para contar, usamos lower() para que toda mayuscula sea convertido en minuscula
+        if letra.lower() in "aeiou":
+            sumando += 1
+
+    return sumando
+
+mi_texto = "Encontremos cuantas vocales existen en este texto"
+result_vocales = contar_vocales(mi_texto)
+print(f"Resultado total vocales: {result_vocales}")
--- Soft_Proyectos_python/App_3_Funciones/02 Ejercicios-Funciones-Condicionales.py
+++ Soft_Proyectos_python/App_3_Funciones/02 Ejercicios-Funciones-Condicionales.py
@@ -32,3 +32,99 @@ def filtro_de_pares(list_numeros):
 
 resultado_pares = filtro_de_pares(list_numbers)
 print(f"Este es el resultado con list comprehesion: {resultado_pares}")
+
+
+ #Crea una funcion que permita calcular el factorial de un numero:
+
+#Primera forma:
+
+#Creamos la funcion y tendra un solo parametro
+def calcular_factorial(numero):
+#validamos si el valor ingresado es un numero entero
+    
+    if type(numero) is int:
+        #Si el numero es 0 o 1 devolvemos 1
+        if numero == 0 or numero == 1:
+             return 1
+        else:
+            factorial = 1
+            #En este for utilizamos un range para hacer un rango entre el 1 y el numero ingresado y el reversed es para obtener el listado en reversa
+            #Si ingresamos un 10, entonces el recorrido usando reversed() sera desde el 10,9,8,7...,1
+            for a in reversed(range(1,numero+1)):
+                factorial = factorial * a
+            return factorial
+    else:
+#En caso de no cumplir con la condicion se muestra un mensaje         
+         return "Error, valor ingresado no es un número"
+resultado_factorial = calcular_factorial(11)
+print(f"El resultado del factorial es: {resultado_factorial}")
+
+
+
+#Segunda forma:
+#usando las funciones incorporadas math factorial:
+#Para ello impoortamos la funcion math
+import math
+
+def mi_factorial(numero):
+     if isinstance(numero,int):
+        return math.factorial(numero)
+     else:
+        return "Error, valor ingresado no es un número"
+
+resultado_fact = mi_factorial(33)
+print(f"El resultado del factorial es: {resultado_fact}")
+
+
+
+
+
+
+
+"""
+Crea una función que determine si un número es primo.
+Numeros primos: Aquellos que se dividen entre 1 y en si mismo
+
+Acá tenemos varias condicionales
+
+1 Validar que numero 1 no es primo (ya que solo tiene 1 divisor)
+2 Validar que numero 2 y 3 son primos de manera rapida
+3 Validar que todos los numero pares despues del 2 no son primos
+4 Ultima condicion donde validamos el resto de números. Lo que buscamos es al numero ingresado elevarlo al cuadrado para poder hacer un recorrido
+  en el rango del 3 al limite para validar
+5 En el recorrido lo haremos partiendo desde el 3 en adelante pero de 2 en 2 es decir (3, 5, 7, 9, ....)
+6 Validamos que el numero ingresado con el valor i del rango nos de residuo 0. Si ninguno se cumple sale del bucle y retornaria que es primo.
+  En caso alguno cumpla con la condicion retornaria que no es primo
+  
+
+"""
+
+def es_primo(numero):
+
+
+#1
+    if numero == 1 :
+        return "no es primo"
+#2
+    elif numero in (2, 3):
+        return "es primo"
+#3
+    elif numero % 2 == 0:
+        return "no es primo"
+#4
+    else:
+        limite = (numero ** 2) + 1
+#5         
+        for i in range(3, limite, 2):
+#6            
+            if numero % i == 0:                
+                return "no es primo"
+
+        return "es primo"       
+        
+
+    
+
+number=27
+resultado_primo = es_primo(number)
+print(f"El numero {number}: {resultado_primo}")
